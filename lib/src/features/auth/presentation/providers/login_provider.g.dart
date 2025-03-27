769b2ad8d78feef1cdad667b5076b11effbe2e72
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-27 13:53:08 +0200

    Updated LoginRepo & Finished Login provider

---
+++ lib/src/features/auth/presentation/providers/login_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginHash() => r'b73e90e668b23250eba6f6d64a0d7d82291ae8d5';
+
+/// See also [Login].
+@ProviderFor(Login)
+final loginProvider = AutoDisposeNotifierProvider<Login, AsyncValue?>.internal(
+  Login.new,
+  name: r'loginProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$loginHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Login = AutoDisposeNotifier<AsyncValue?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
