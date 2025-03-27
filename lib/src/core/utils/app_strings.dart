2acd84270a7d7389304bf2e3e1da2689b642ce51
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-27 18:02:56 +0200

    Built new extensions  - Built ShowCustomAnimatedDialog extension  - Built ShowLoadingDialog extension

--- lib/src/core/utils/app_strings.dart
+++ lib/src/core/utils/app_strings.dart
@@ -1,3 +1,19 @@
 class AppStrings {
   static const String appTitle = 'DocDoc';
+  static const String onboardingTitle = 'Best Doctor\nAppointment App';
+  static const String onboardingDescription =
+      'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.';
+  static const String getStarted = 'Get Started';
+  static const String welcomeBack = 'Welcome Back';
+  static const String loginViewSubTitle =
+      "We're excited to have you back, can't wait to see what you've been up to since you last logged in.";
+  static const String opps = 'Opps!';
+  static const String success = 'Success!';
+  static const String warning = 'Warning';
+  static const String byLoggingYouAgree = 'By logging, you agree to our';
+  static const String termsAndConditions = 'Terms & Conditions';
+  static const String and = 'and';
+  static const String privacyPolicy = 'Privacy Policy';
+  static const String dontHavAcc = "Don't have an account?";
+  static const String signUp = 'Sign up';
 }
