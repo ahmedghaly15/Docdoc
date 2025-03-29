2117287978663ef789b9e5cc7789c7ffe0294f26
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-29 16:17:37 +0200

    Finished CacheHelper

---
+++ lib/src/core/helpers/cache_keys.dart
@@ -0,0 +1,8 @@
+bool isUserLoggedIn = false;
+bool isOnboardingVisited = false;
+
+class CacheKeys {
+  static const String user = 'user';
+  static const String userPass = 'userPass';
+  static const String onboarding = 'onboarding';
+}
