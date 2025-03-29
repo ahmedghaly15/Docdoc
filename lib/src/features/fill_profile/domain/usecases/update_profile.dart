f012ff714fe3c1cbbd621e21dbaed49f8800f073
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-29 13:55:48 +0200

    Finished UpdateProfile use case

---
+++ lib/src/features/fill_profile/domain/usecases/update_profile.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/api/api_result.dart';
+import '../../../../core/usecases/api_usecase.dart';
+import '../../../auth/data/models/auth_response.dart';
+import '../../../auth/data/models/register_request_body.dart';
+import '../../data/repos/fill_profile_repo.dart';
+
+final updateProfileProvider = Provider<UpdateProfile>((ref) {
+  final fillProfileRepo = ref.watch(fillProfileRepoProvider);
+  return UpdateProfile(fillProfileRepo);
+});
+
+class UpdateProfile implements ApiUseCase<AuthResponse, RegisterRequestBody> {
+  final FillProfileRepo _fillProfileRepo;
+
+  UpdateProfile(this._fillProfileRepo);
+
+  @override
+  Future<ApiResult<AuthResponse>> call(RegisterRequestBody params) {
+    return _fillProfileRepo.updateProfile(params);
+  }
+}
