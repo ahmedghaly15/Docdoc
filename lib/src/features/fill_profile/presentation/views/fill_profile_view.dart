7a62360a98dce53425f6c2e2d4e515391425530c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-29 04:54:24 +0200

    Finished fill-profile feature's UI

---
+++ lib/src/features/fill_profile/presentation/views/fill_profile_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/utils/app_constants.dart';
+import '../../../../core/utils/app_strings.dart';
+import '../../../auth/presentation/views/widgets/auth_subtitle.dart';
+import '../../../auth/presentation/views/widgets/auth_title.dart';
+import 'widgets/fill_profile_form.dart';
+import 'widgets/submit_consumer_button.dart';
+
+class FillProfileView extends StatelessWidget {
+  const FillProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: AppConstants.screenHorizontalPadding,
+          ),
+          child: CustomScrollView(
+            slivers: [
+              const SliverToBoxAdapter(
+                child: AuthTitle(title: AppStrings.fillYourProfile),
+              ),
+              const SliverToBoxAdapter(
+                child: AuthSubTitle(
+                  subTitle: AppStrings.fillProfileViewDescription,
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: EdgeInsets.only(top: 32.h, bottom: 16.h),
+                  child: const FillProfileForm(),
+                ),
+              ),
+              const SliverToBoxAdapter(
+                child: SubmitConsumerButton(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
