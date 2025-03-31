2be07699c0be6574a39fef7faf37b390603c3aca
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-31 02:39:05 +0200

    Refactored HomeView widget

--- lib/src/features/home/presentation/views/home_view.dart
+++ lib/src/features/home/presentation/views/home_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:docdoc/src/core/widgets/my_sized_box.dart';
-import 'package:docdoc/src/features/home/presentation/views/widgets/doctor_speciality_list_view.dart';
-import 'package:docdoc/src/features/home/presentation/views/widgets/doctors_list_view.dart';
-import 'package:docdoc/src/features/home/presentation/views/widgets/find_near_by_doctor_blue_container.dart';
-import 'package:docdoc/src/features/home/presentation/views/widgets/home_custom_app_bar.dart';
-import 'package:docdoc/src/features/home/presentation/views/widgets/see_all_row.dart';
+import '../../../../core/utils/app_strings.dart';
+import 'widgets/doctor_speciality_list_view.dart';
+import 'widgets/find_near_by_doctor_blue_container.dart';
+import 'widgets/home_custom_app_bar.dart';
+import 'widgets/recommended_doctors_sliver_list.dart';
+import 'widgets/see_all_row.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -15,20 +15,29 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          margin: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              HomeCustomAppBar(),
-              FindNearByDoctorBlueContainer(),
-              MySizedBox.height24,
-              DoctorSpecialitySeeAll(),
-              MySizedBox.height16,
-              DoctorSpecialityListView(),
-              MySizedBox.height23,
-              DoctorsListView(),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
+          child: CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            slivers: [
+              const SliverToBoxAdapter(child: HomeCustomAppBar()),
+              const SliverToBoxAdapter(
+                child: FindNearByDoctorBlueContainer(),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: EdgeInsets.only(top: 24.h),
+                  child: const SeeAllRow(title: AppStrings.doctorSpeciality),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  height: 86.h,
+                  margin: EdgeInsets.only(top: 16.h, bottom: 23.h),
+                  child: const DoctorSpecialityListView(),
+                ),
+              ),
+              const RecommendedDoctorsSliverList(),
             ],
           ),
         ),
