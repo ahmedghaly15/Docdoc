5d7364be2d86191f6c8080fcbbba3a160a0ea1c7
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-31 02:37:36 +0200

    Refactored DoctorSpecialityListView & Upated PrimaryButton widgets

--- lib/src/features/home/presentation/views/widgets/doctor_speciality_list_view.dart
+++ lib/src/features/home/presentation/views/widgets/doctor_speciality_list_view.dart
@@ -1,43 +1,39 @@
-import 'package:docdoc/src/config/themes/app_colors.dart';
-import 'package:docdoc/src/config/themes/app_text_styles.dart';
-import 'package:docdoc/src/core/utils/app_assets.dart';
-import 'package:docdoc/src/core/widgets/my_sized_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../../config/themes/app_colors.dart';
+import '../../../../../config/themes/app_text_styles.dart';
+import '../../../../../core/utils/app_assets.dart';
+import '../../../../../core/widgets/my_sized_box.dart';
 
 class DoctorSpecialityListView extends StatelessWidget {
   const DoctorSpecialityListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 86.h,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) => Padding(
-          padding: EdgeInsetsDirectional.only(start: index == 0 ? 0 : 24.w),
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 28.r,
-                backgroundColor: AppColors.primaryColor,
-                child: SvgPicture.asset(
-                  AppAssets.svgsGeneralSpeciality,
-                  height: 24.h,
-                  width: 24.w,
-                ),
-              ),
-              MySizedBox.height12,
-              Text(
-                'Speciality',
-                style: AppTextStyles.font12DarkBlueRegular,
-              ),
-            ],
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: 10,
+      itemBuilder: (context, index) => Column(
+        spacing: 12.h,
+        children: [
+          CircleAvatar(
+            radius: 28.r,
+            backgroundColor: AppColors.primaryColor,
+            child: SvgPicture.asset(
+              AppAssets.svgsGeneralSpeciality,
+              height: 24.h,
+              width: 24.w,
+            ),
           ),
-        ),
+          Text(
+            'Speciality',
+            style: AppTextStyles.font12DarkBlueRegular,
+          ),
+        ],
       ),
+      separatorBuilder: (_, __) => MySizedBox.width24,
     );
   }
 }
