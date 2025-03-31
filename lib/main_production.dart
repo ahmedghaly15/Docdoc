67432ba61713b31b680cff0688eaea8e4adf735a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-31 02:53:17 +0200

    Updated main method & AppRouter

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,14 +1,13 @@
-import 'package:docdoc/dependency_injection.dart';
-import 'package:docdoc/src/core/utils/functions/check_if_onboarding_is_visited.dart';
-import 'package:docdoc/src/core/utils/functions/check_if_user_logged_in.dart';
-import 'package:docdoc/src/docdoc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'src/core/utils/functions/check_if_onboarding_is_visited.dart';
+import 'src/core/utils/functions/check_if_user_logged_in.dart';
+import 'src/docdoc_app.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setupGetIt();
   // To fix texts being hidden bug in release mode
   await ScreenUtil.ensureScreenSize();
   await checkIfOnboardingIsVisited();
