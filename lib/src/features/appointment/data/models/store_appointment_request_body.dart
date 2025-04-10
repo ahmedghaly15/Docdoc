27150e5a5069bc57ca5a337f2240b11e0c59bb82
Ahmed Ghaly <108659381+ahmedghaly15@users.noreply.github.com>
2025-04-10 12:41:26 +0200

    Built StoreAppointmentRequestBody model

---
+++ lib/src/features/appointment/data/models/store_appointment_request_body.dart
@@ -0,0 +1,20 @@
+part 'store_appointment_request_body.g.dart';
+
+@JsonSerializable()
+class StoreAppointmentRequestBody{
+  @JsonKey(name: 'doctor_id')
+  final int doctorId;
+  @JsonKey(name: 'start_time')
+  final String startTime;
+  final String notes;
+  
+  StoreAppointmentRequestBody({
+    required this.doctorId,
+    required this.startTime,
+    required this.notes,
+  });
+  
+  factory StoreAppointmentRequestBody.fromJson(Map<String, dynamic> json) => _$StoreAppointmentRequestBodyFromJson(json);
+  
+  Map<String, dynamic> toJson() => _$StoreAppointmentRequestBodyToJson(this);
+}
