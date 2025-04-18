41863133b0c6db789ced7f1b7796083a7398beca
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-04-18 23:39:45 +0200

    Refactored RecommendedDoctorsSliverListConsumer widget  - Built DoctorItem widget

--- lib/src/core/widgets/recommended_doctors_sliver_list_consumer.dart
+++ lib/src/core/widgets/recommended_doctors_sliver_list_consumer.dart
@@ -1,14 +1,12 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:skeletonizer/skeletonizer.dart';
 
-import '../../config/themes/app_text_styles.dart';
 import '../../features/doctors/presentation/providers/doctors_providers.dart';
 import '../models/doctor.dart';
 import 'custom_error_widget.dart';
 import 'custom_skeletonizer.dart';
+import 'doctor_item.dart';
 import 'my_sized_box.dart';
 
 enum RecommendedDoctorsCount {
@@ -68,46 +66,7 @@ class RecommendedDoctorsSliverList extends StatelessWidget {
       itemCount: itemCount,
       itemBuilder: (_, index) => SizedBox(
         height: 126.h,
-        child: Row(
-          spacing: 16.w,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Flexible(
-              child: Skeleton.leaf(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12.r),
-                  child: CachedNetworkImage(
-                    imageUrl:
-                        'https://img.freepik.com/free-photo/smiling-doctor-with-strethoscope-isolated-grey_651396-974.jpg?t=st=1744886153~exp=1744889753~hmac=307ac7c212d04de889b9fe13c9b420ef96a1c85d0f6e26d6d083b686640d4cba&w=1380',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Column(
-                spacing: 8.h,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    doctors?[index].name ?? 'Doctor name',
-                    style: AppTextStyles.font16DarkBlueBold,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Text(
-                    '${doctors?[index].specialization.name} | ${doctors?[index].address}',
-                    style: AppTextStyles.font12GreyMedium,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+        child: DoctorItem(doctor: doctors?[index]),
       ),
       separatorBuilder: (_, __) => MySizedBox.height16,
     );
