cbcc38931ce44c1bf6a626417e03de3c025d1a19
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-04-18 15:30:35 +0200

    Refactored SpecializationsView widget

--- lib/src/features/specializations/presentation/views/specializations_view.dart
+++ lib/src/features/specializations/presentation/views/specializations_view.dart
@@ -2,15 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../core/models/specialization.dart';
 import '../../../../core/utils/app_constants.dart';
 import '../../../../core/utils/app_strings.dart';
 import '../../../../core/widgets/adaptive_refresh_indicator.dart';
-import '../../../../core/widgets/custom_error_widget.dart';
-import '../../../../core/widgets/custom_skeletonizer.dart';
 import '../../../../core/widgets/custom_sliver_app_bar.dart';
-import '../../../../core/widgets/specialization_widget.dart';
 import '../providers/specializations_providers.dart';
+import 'widgets/specializations_sliver_grid_consumer.dart';
 
 class SpecializationsView extends ConsumerWidget {
   const SpecializationsView({super.key});
@@ -35,53 +32,6 @@ class SpecializationsView extends ConsumerWidget {
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class SpecializationsSliverGridConsumer extends ConsumerWidget {
-  const SpecializationsSliverGridConsumer({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final specializations = ref.watch(fetchSpecializationsProvider);
-    return specializations.when(
-      data: (data) => SpecializationsSliverGrid(
-        specializations: data.data,
-      ),
-      error: (error, _) => SliverFillRemaining(
-        hasScrollBody: false,
-        child: CustomErrorWidget(
-          error: error.toString(),
-          onPressed: () => ref.refresh(fetchSpecializationsProvider),
-        ),
-      ),
-      loading: () => const CustomSkeletonizer(
-        type: CustomSkeletonizerType.sliver,
-        child: SpecializationsSliverGrid(),
-      ),
-    );
-  }
-}
-
-class SpecializationsSliverGrid extends StatelessWidget {
-  const SpecializationsSliverGrid({super.key, this.specializations});
-
-  final List<Specialization>? specializations;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverGrid.builder(
-      itemCount: specializations?.length ?? 9,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        childAspectRatio: 80 / 117,
-        crossAxisCount: 3,
-        mainAxisSpacing: 32.h,
-        crossAxisSpacing: 36.w,
-      ),
-      itemBuilder: (_, index) => SpecializationWidget(
-        specialization: specializations?[index],
       ),
     );
   }
